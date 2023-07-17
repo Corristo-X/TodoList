6bf7309940634ac6be0b0a9fe2caedcb546378b1
Daniel <daniel@Ubuntu22.10.myguest.virtualbox.org>
2023-07-17 23:59:22 +0200

    [TL-5] Routing Update

--- TodoList/src/Security/LoginFormAuthenticator.php
+++ TodoList/src/Security/LoginFormAuthenticator.php
@@ -44,17 +44,18 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
+        $targetPath = $this->getTargetPath($request->getSession(), $firewallName);
+    
+        if (!$targetPath) {
+            $targetPath = $this->urlGenerator->generate('home');
         }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    
+        return new RedirectResponse($targetPath);
     }
+    
 
     protected function getLoginUrl(Request $request): string
     {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+        return $this->urlGenerator->generate('app_login');
     }
 }
